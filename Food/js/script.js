ec28b3bfddc09344d26ad8a8c79be6636fb710c1
Artem <artem.danko.2k18@gmail.com>
2021-09-23 21:02:12 +0300

    Added timer

--- Food/js/script.js
+++ Food/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+   // Tabs
    const tabs = document.querySelectorAll('.tabheader__item'),
       tabsContent = document.querySelectorAll('.tabcontent'),
       tabsParent = document.querySelector('.tabheader__items');
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
    function hideTabContent() {
       tabsContent.forEach((item) => {
          item.classList.add('hide');
-			item.classList.remove('show', 'fade');
+         item.classList.remove('show');
       });
 
       tabs.forEach((tab) => {
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
    function showTabContent(i = 0) {
       tabsContent[i].classList.add('show', 'fade');
-		tabsContent[i].classList.remove('hide');
+      tabsContent[i].classList.remove('hide');
       tabs[i].classList.add('tabheader__item_active');
    }
 
@@ -28,11 +29,61 @@ document.addEventListener('DOMContentLoaded', () => {
       const target = e.target;
       if (target && target.classList.contains('tabheader__item')) {
          tabs.forEach((item, i) => {
-				if (target == item) {
-					hideTabContent();
-					showTabContent(i);
-				}
-			});
+            if (target == item) {
+               hideTabContent();
+               showTabContent(i);
+            }
+         });
       }
    });
+
+   // Timer
+   const deadLine = '2021-10-23';
+
+   function getTiemRemaining(endTime) {
+      const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 3600 *24)),
+            hours = Math.floor((t / (1000 * 3600)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+      return {
+         total: t,
+         days,
+         hours,
+         minutes,
+         seconds,
+      };
+   }
+
+   function setClock (selector,endtime) {
+      const timer = document.querySelector(selector),
+         days = timer.querySelector('#days'),
+         hours = timer.querySelector('#hours'),
+         minutes = timer.querySelector('#minutes'),
+         seconds = timer.querySelector('#seconds'),
+         timeInterval = setInterval(updateClock,1000);
+      
+      function addZero(num) {
+         if(num >= 0 && num < 10) {
+            return `0${num}`
+         } else {
+            return num;
+         }
+      }
+      
+      updateClock();
+      function updateClock() {
+         const t = getTiemRemaining(endtime);
+
+         days.innerHTML = addZero(t.days);
+         hours.innerHTML = addZero(t.hours);
+         minutes.innerHTML = addZero(t.minutes);
+         seconds.innerHTML = addZero(t.seconds);
+         
+         if (t.total <= 0)  {
+            clearInterval(timeInterval);
+         }
+      }
+   }
+   setClock('.timer', deadLine);
 });
