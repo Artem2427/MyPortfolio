909d196f605470ae872b24517e63b9b54fa4a7f1
Artem <artem.danko.2k18@gmail.com>
2021-10-02 12:12:24 +0300

    Send message on server

--- Food/js/script.js
+++ Food/js/script.js
@@ -42,10 +42,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
    function getTiemRemaining(endTime) {
       const t = Date.parse(endTime) - Date.parse(new Date()),
-            days = Math.floor(t / (1000 * 3600 *24)),
-            hours = Math.floor((t / (1000 * 3600)) % 24),
-            minutes = Math.floor((t / (1000 * 60)) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+         days = Math.floor(t / (1000 * 3600 * 24)),
+         hours = Math.floor((t / (1000 * 3600)) % 24),
+         minutes = Math.floor((t / (1000 * 60)) % 60),
+         seconds = Math.floor((t / 1000) % 60);
       return {
          total: t,
          days,
@@ -55,22 +55,22 @@ document.addEventListener('DOMContentLoaded', () => {
       };
    }
 
-   function setClock (selector,endtime) {
+   function setClock(selector, endtime) {
       const timer = document.querySelector(selector),
          days = timer.querySelector('#days'),
          hours = timer.querySelector('#hours'),
          minutes = timer.querySelector('#minutes'),
          seconds = timer.querySelector('#seconds'),
-         timeInterval = setInterval(updateClock,1000);
-      
+         timeInterval = setInterval(updateClock, 1000);
+
       function addZero(num) {
-         if(num >= 0 && num < 10) {
-            return `0${num}`
+         if (num >= 0 && num < 10) {
+            return `0${num}`;
          } else {
             return num;
          }
       }
-      
+
       updateClock();
       function updateClock() {
          const t = getTiemRemaining(endtime);
@@ -79,11 +79,197 @@ document.addEventListener('DOMContentLoaded', () => {
          hours.innerHTML = addZero(t.hours);
          minutes.innerHTML = addZero(t.minutes);
          seconds.innerHTML = addZero(t.seconds);
-         
-         if (t.total <= 0)  {
+
+         if (t.total <= 0) {
             clearInterval(timeInterval);
          }
       }
    }
    setClock('.timer', deadLine);
+
+   // Modal
+
+   const modalBtns = document.querySelectorAll('[data-modal]'),
+      modalWindow = document.querySelector('.modal'),
+      closeModalBtn = document.querySelector('.modal__close');
+
+   function showModal() {
+      modalWindow.classList.remove('hide');
+      modalWindow.classList.add('show');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);
+   }
+   modalBtns.forEach((item) => {
+      item.addEventListener('click', showModal);
+   });
+
+   function closeModal() {
+      modalWindow.classList.remove('show');
+      modalWindow.classList.add('hide');
+      document.body.style.overflow = '';
+   }
+   closeModalBtn.addEventListener('click', closeModal);
+
+   modalWindow.addEventListener('click', (e) => {
+      if (e.target === modalWindow) {
+         closeModal();
+      }
+   });
+
+   document.addEventListener('keydown', (e) => {
+      // console.dir(e.code);
+      if (e.code === 'Escape' && modalWindow.classList.contains('show')) {
+         closeModal();
+      }
+   });
+
+   const modalTimerId = setTimeout(showModal, 5000);
+
+   function showModalByScroll() {
+      if (
+         window.pageYOffset + document.documentElement.clientHeight >=
+         document.documentElement.scrollHeight
+      ) {
+         showModal();
+         window.removeEventListener('scroll', showModalByScroll);
+      }
+   }
+
+   window.addEventListener('scroll', showModalByScroll);
+
+   // Cards
+   class Card {
+      constructor(src, alt, title, text, price, parentSelector, ...classes) {
+         this.src = src;
+         this.alt = alt;
+         this.price = price;
+         this.title = title;
+         this.text = text;
+         this.classes = classes;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 27;
+         this.changeToUAH();
+      }
+
+      changeToUAH() {
+         this.price = +this.price * this.transfer;
+      }
+
+      render() {
+         const div = document.createElement('div');
+
+         if (this.classes.length === 0) {
+            this.div = 'menu__item';
+            div.classList.add(this.div);
+         } else {
+            this.classes.forEach((item) => div.classList.add(item));
+         }
+
+         div.innerHTML = `
+                     <img src=${this.src} alt=${this.alt}>
+                     <h3 class="menu__item-subtitle">${this.title}</h3>
+                     <div class="menu__item-descr">${this.text}</div>
+                     <div class="menu__item-divider"></div>
+                     <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                     </div>`;
+         this.parent.append(div);
+      }
+   }
+
+   new Card(
+      'img/tabs/vegy.jpg',
+      'vegy',
+      'Меню "Фитнес"',
+      `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
+                        овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
+                        ценой и высоким качеством!`,
+      9,
+      '.menu__field .container',
+      'menu__item'
+   ).render();
+
+   new Card(
+      'img/tabs/elite.jpg',
+      'elite',
+      'Меню “Премиум”',
+      `В меню “Премиум” мы используем не только красивый дизайн упаковки, но
+                        и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
+                        в ресторан!`,
+      14,
+      '.menu__field .container',
+      'menu__item'
+   ).render();
+
+   new Card(
+      'img/tabs/post.jpg',
+      'post',
+      'Меню "Постное"',
+      `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие
+                        продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
+                        количество белков за счет тофу и импортных вегетарианских стейков. </div>`,
+      21,
+      '.menu__field .container',
+      'menu__item'
+   ).render();
+
+   // Forms
+
+   const forms = document.querySelectorAll('form');
+
+   const message = {
+      loading: 'Загрузка',
+      success: 'Спасибо! Скоро мы с вами свяжемся',
+      failure: 'Что-то пошло не так...',
+   };
+
+   forms.forEach((item) => {
+      postData(item);
+   });
+
+   function postData(form) {
+      form.addEventListener('submit', (e) => {
+         e.preventDefault();
+
+         const statusMessage = document.createElement('div');
+         statusMessage.classList.add('status');
+         statusMessage.textContent = message.loading;
+         form.append(statusMessage);
+
+         const request = new XMLHttpRequest();
+         request.open('POST', 'server.php');
+
+         request.setRequestHeader(
+            'Content-type',
+            'application/json; charset-utf-8'
+         );
+         const formData = new FormData(form);
+
+         const object = {};
+
+         formData.forEach(function (value, key) {
+            object[key] = value;
+         });
+
+         const json = JSON.stringify(object);
+
+         request.send(json);
+         // request.send(formData);
+
+         request.addEventListener('load', () => {
+            if (request.status === 200) {
+               console.log(request.response);
+               statusMessage.textContent = message.success;
+               form.reset();
+               setTimeout(() => {
+                  statusMessage.remove();
+                  // closeModal();
+               }, 2000);
+            } else {
+               statusMessage.textContent = message.failure;
+            }
+         });
+      });
+   }
 });
